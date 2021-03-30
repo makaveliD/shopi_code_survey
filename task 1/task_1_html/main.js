236f13ece0a812b4b2af0bdce2f1c5c3c47f3961
Volodymyr Zabolotny <vovazabalotny@gmail.com>
2021-03-30 16:13:00 +0300

    initial commit

---
+++ task 1/task_1_html/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $('#slideshow').slick({
+        slidesToShow: 3
+    });
+})
---
+++ task 2/task_2_1.sql
@@ -0,0 +1,1 @@
+SELECT skus.color, skus.size FROM product LEFT JOIN skus ON skus.product_id = product.id WHERE product.published_at < CURRENT_TIMESTAMP AND ( product.pre_sale IS NULL OR product.pre_sale > CURRENT_TIMESTAMP )
